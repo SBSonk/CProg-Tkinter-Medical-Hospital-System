7e3370efc13f28b0d531c85ca25b99a461917f90
Stanley Buenaventura <105530737+SBSonk@users.noreply.github.com>
2025-04-01 12:24:07 +0800

    login window test

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-from flask import Flask
 from models import db, bcrypt
 import tkinter as tk
 from frames.login import Login
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-
-bcrypt.init_app(app)
-db.init_app(app)
+# bcrypt.init_app(app)
+# db.init_app(app)
 
 # todo: need to create database
 root = tk.Tk()
 
 # todo: rerun at home
 login_window = Login(root)
-login_window.pack()
 
 if __name__ == '__main__':
-    app.run()
     root.mainloop()
