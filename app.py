e8a13777cbf85cebf892568134e9a38850ab42de
PatheticDart <114214218+PatheticDart@users.noreply.github.com>
2025-04-08 20:55:55 +0800

    patient profile WIP

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session, sessionmaker
 import frames
 import frames.landing
 import frames.login
+import frames.patient_profile
 import frames.register
 import frames.main_menu
 import fractions
@@ -21,12 +22,15 @@ session = Session()
 def main():
     # Register Windows
     add_window("login", frames.login.Login, (session, None, lambda: switch_to_window('main')))
+    add_window("forget_password", frames.forget_password.ForgetPassword)
+    # add_window("reset_password", frames.reset_password.ResetPassword)
     add_window("register", frames.register.Register, (session,))
     add_window("main", frames.main_menu.MainMenu)
     add_window("landing", frames.landing.LandingFrame)
+    add_window("patient_profile", frames.patient_profile.PatientProfile)
 
     # Open first window
-    switch_to_window('landing')
+    switch_to_window('patient_profile')
 
 
 if __name__ == "__main__":
